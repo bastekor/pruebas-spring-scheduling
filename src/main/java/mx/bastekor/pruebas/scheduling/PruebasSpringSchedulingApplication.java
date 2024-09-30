91373dd81f4af27c83060acc9adb8e86a09a78c0
bastekor <bastekor.dev@outlook.com>
2024-09-30 13:08:49 -0600

    Se inicializa el proyecto

---
+++ src/main/java/mx/bastekor/pruebas/scheduling/PruebasSpringSchedulingApplication.java
@@ -0,0 +1,13 @@
+package mx.bastekor.pruebas.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasSpringSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasSpringSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/bastekor/pruebas/scheduling/PruebasSpringSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bastekor.pruebas.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasSpringSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
